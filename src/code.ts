ac5057c51f9f6f374489979370aab605cb6d813a
puppets101 <oscand92@gmail.com>
2020-11-24 16:08:53 +0100

    Add working functionality to jumping alfons code

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,51 @@
-let x = 100.9208037;
+window.addEventListener("load", main);
 
-console.log(x.toFixed(1));
+let character: HTMLElement | null;
+let position = { bottom: 0, left: 0 };
+
+function main() {
+  character = document.querySelector(".character");
+  addEventListeners();
+
+  runGameLoop();
+}
+
+function addEventListeners() {
+  window.addEventListener("keydown", handleUserInput);
+}
+
+function runGameLoop() {
+  setInterval(function () {
+    applyGravity();
+    renderCharacter();
+  }, 10);
+}
+
+function applyGravity() {
+  position.bottom -= 0.1;
+  if (position.bottom < 0) {
+    position.bottom = 0;
+  }
+}
+
+function renderCharacter() {
+  if (character) {
+    character.style.bottom = position.bottom + "%";
+    character.style.left = position.left + "%";
+  }
+}
+
+function handleUserInput(event: KeyboardEvent) {
+  switch (event.key) {
+    case "ArrowLeft":
+      position.left -= 1;
+      break;
+    case "ArrowRight":
+      position.left += 1;
+      break;
+    case " ":
+      if (position.bottom === 0) {
+        position.bottom += 10;
+      }
+  }
+}
